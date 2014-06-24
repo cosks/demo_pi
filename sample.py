16692f4bcd502d3b791c97e7e0b1f88cb155688f
kkorenev <kkorenev@hotmail.com>
2014-06-24 16:37:41 +0100

    Testing

--- sample.py
+++ sample.py
@@ -1,9 +1,15 @@
 import urllib
-urls = ['http://google.com', 'http://mail.ru', 'http://yandex.ru']
+import re
+
+urls = ['http://google.com', 'http://mail.ru', 'http://youtube.com']
 i = 0
+regex = '<title>(.+?)</title>'
+pattern = re.compile(regex)
 
 while i<len(urls):
 	htmlfile = urllib.urlopen(urls[i])
 	htmltext = htmlfile.read()
-	print htmltext
+	titles = re.findall(pattern,htmltext)
+	# print htmltext [0:100]
+	print titles
 	i+=1
