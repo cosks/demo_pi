f3ddc51173a6bdef7df5ca256b88fd278f86cd0d
kkorenev <kkorenev@hotmail.com>
2014-07-02 20:02:31 +0100

    Downloading less for better efficiency

--- goog_scrape.py
+++ goog_scrape.py
@@ -1,7 +1,10 @@
 import urllib
 import re
 
-htmlfile = urllib.urlopen('https://www.google.co.uk/finance?q=AAPL')
+#htmlfile = urllib.urlopen('https://www.google.co.uk/finance?q=AAPL')
+
+# making a strait forward request to the server without the webpage
+htmlfile = urllib.urlopen('https://www.google.co.uk/finance/getprices?q=AAPL&x=NASD&i=120&p=25m&f=c&df=cpct&auto=1&ts=1404326982484&ei=CVS0U_DUHc-uwAPt9YCYCg')
 htmltext = htmlfile.read()
 
 regex = '<span id="ref_[^.]*_l">(.+?)</span>'
@@ -10,4 +13,6 @@ pattern = re.compile(regex)
 
 results = re.findall(pattern,htmltext)
 
-print results
+#print results
+
+print htmltext.split()[len(htmltext.split())-1]
